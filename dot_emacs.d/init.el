160a623b34559e34b87b11eabaf2b570da3fa4eb
bvtuhan <jjackson.stormm@gmail.com>
2026-06-27 11:04:39 +0200

    refactor(emacs): Update README

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -63,10 +63,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(ace-window adaptive-wrap apheleia cape cider clojure-ts-mode copilot
+                corfu-terminal crdt dape diredfl dirvish doom-modeline
+                embark-consult evil-collection evil-commentary evil-goggles
+                evil-numbers evil-org evil-surround evil-tex flycheck-eglot
+                flycheck-posframe general hl-todo jinx marginalia orderless
+                org-download org-fragtog org-noter pdf-tools rustic sly
+                treesit-auto undo-fu undo-fu-session vertico vundo
+                yasnippet-snippets yellowbeans-theme zig-ts-mode))
  '(package-vc-selected-packages
-   '((dirvish :url "https://github.com/latiagertrutis/dirvish" :rev
-              :newest :lisp-dir ".")
+   '((dirvish :url "https://github.com/latiagertrutis/dirvish" :rev :newest
+              :lisp-dir ".")
      (yellowbeans-theme :url
                         "https://github.com/bvtuhan/yellowbeans-theme-emacs"))))
 
