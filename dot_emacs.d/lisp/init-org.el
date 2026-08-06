28276d4c99852e4972ad95ee0d7b13879dd6da3e
bvtuhan <jjackson.stormm@gmail.com>
2026-08-06 10:58:46 +0200

    feat(emacs): add org-pretty-entities

--- dot_emacs.d/lisp/init-org.el
+++ dot_emacs.d/lisp/init-org.el
@@ -29,6 +29,7 @@
   (org-startup-folded nil)
   (org-tags-column 0)
   (org-image-actual-width nil)
+  (org-pretty-entities t)
   (org-refile-targets '((nil :maxlevel . 3)
                         (org-agenda-files :maxlevel . 3)))
   (org-refile-use-outline-path 'file)
