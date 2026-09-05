9eda96c8609f304f95a37234cd2b4eb88c7c4048
bvtuhan <jjackson.stormm@gmail.com>
2026-09-05 23:06:00 +0200

    chore(emacs): remove vc for org-draw

--- dot_emacs.d/lisp/init-org.el
+++ dot_emacs.d/lisp/init-org.el
@@ -198,7 +198,6 @@
     (edraw-org-setup-exporter)))
 
 (use-package org-draw
-  :vc (:url "https://github.com/larrasket/org-draw")
   :commands (org-draw org-draw-edit org-draw-setup)
   :bind (:map org-mode-map
               ("C-c d d" . org-draw)
