199241710558a96c5675843436acaf4e2f648e89
bvtuhan <jjackson.stormm@gmail.com>
2026-09-05 15:41:47 +0200

    chore(emacs): make dirvish to refer main upstream

--- dot_emacs.d/lisp/init-dired.el
+++ dot_emacs.d/lisp/init-dired.el
@@ -69,25 +69,8 @@
   (unless (display-graphic-p)
     (setq nerd-icons-color-icons nil)))
 
-(unless (package-installed-p 'dirvish)
-  (package-vc-install
-   '(dirvish
-     :url "https://github.com/latiagertrutis/dirvish"
-     :rev :newest
-     :lisp-dir ".")))
-
-;; we need this for dirvish
-(eval-and-compile
-  (let* ((dirvish-dir (expand-file-name "dirvish" package-user-dir))
-         (ext-dir (expand-file-name "extensions" dirvish-dir)))
-    (add-to-list 'load-path dirvish-dir)
-    (add-to-list 'load-path ext-dir)))
-
 ;; please rm -rf ~/.emacs.d/elpa/dirvish first
 (use-package dirvish
-  ;; main upstream is dead
-  :vc (:url "https://github.com/latiagertrutis/dirvish"
-            :rev :newest)
   :custom
   (dirvish-cache-dir (expand-file-name "dirvish/" user-emacs-directory))
   (dirvish-reuse-session 'open)
